07d45d6c7752e9ca43a8bdeafa4965d7a0150fbc
Caique24-01 <caiqueleme100@gmail.com>
2021-05-04 18:46:13 -0300

    Projeto atualizado com implemantação do nome do contato e foto

--- projetocordova/aulacontatos/www/js/index.js
+++ projetocordova/aulacontatos/www/js/index.js
@@ -1,4 +1,7 @@
 let app = { 
+
+    contatoSelecionado : null,
+
     inicializar: function(){
         console.log("iniciar");
         document.addEventListener('deviceready', app.onMyDeviceReady, false);
@@ -8,7 +11,7 @@ let app = {
         console.log('######Running cordova-' + cordova.platformId + '@' + cordova.version);
         document.getElementById(" bntSelecionaContato").addEventListener('click', app.selecionarContato)
     },
-    retornoAoPrimeiroPlano: function(){
+    retornoAoPrimeiroPlano: function(onResumeEvent){
         console.log("####retorno Ao Primeiro Plano");
     },
     selecionarContato : function(){
@@ -16,8 +19,19 @@ let app = {
         navigator.contacts.pickContact(function(c){
             console.log("#####Contato Selecionado");
             console.log(c);
-        },
-        function(err){
+            app.contatoSelecionado = c;
+            console.log(c.displayName);
+            let spanElement = document.getElementById("nomeDoContato").innerHTML = c.displayName;
+            console.log(spanElement);
+
+            console.log(c.photos)  
+            let imgElement = document.getElementById("imgDoContato");
+            console.log("Tag Img");
+            console.log(imgElement);
+            console.log("Value do obj Imagem do array photos");
+            console.log(c.photos[0].value);
+            imgElement.src = c.photos[0].value;     
+        },function(err){
             console.log("#####Contato não Selecionado");
             console.log(err);
         });
